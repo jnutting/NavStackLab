680c014a3a984c6c3e40361c75875f60d7dd8220
Jack Nutting <jacknutting@mac.com>
2022-12-12 15:32:49 +0100

    Initial Commit

---
+++ NavStackLab/NavStackLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackLabApp.swift
+//  NavStackLab
+//
+//  Created by JN on 2022-12-12.
+//
+
+import SwiftUI
+
+@main
+struct NavStackLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
